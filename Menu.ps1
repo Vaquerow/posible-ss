97c2ef591eca3ecbb46ea142dd77c99618cae8af
Vaquerow <vaquerounai47@gmail.com>
2025-07-22 18:46:17 +0200

    Add files via upload

---
+++ Menu.ps1
@@ -0,0 +1,57 @@
+Clear-Host
+
+function Mostrar-Menu {
+    Write-Host ""
+    Write-Host "======= MENÚ DE SCRIPTS ========" -ForegroundColor DarkGreen
+    Write-Host "1. Analizador de Jars"
+    Write-Host "2. Analizador de Exes"
+    Write-Host "3. EN DESAROLLO"
+    Write-Host "4. Analizador de DLLs (EN DESAROLLO)"
+    Write-Host "0. Salir"
+    Write-Host "===============================" -ForegroundColor DarkGreen
+}
+
+function Ejecutar-Script($url) {
+    try {
+        $tempPath = "$env:TEMP\temp_script_$(Get-Random).ps1"
+        Invoke-WebRequest -Uri $url -OutFile $tempPath -UseBasicParsing
+        Write-Host ""
+        Write-Host "--- Ejecutando script ---" -ForegroundColor DarkBlue
+        Write-Host ""
+
+        . $tempPath
+
+        Remove-Item $tempPath -Force
+    }
+    catch {
+        Write-Host "`n✖ Fallo al usar script:`n$($_.Exception.Message)" -ForegroundColor DarkRed
+    }
+}
+
+$seguir = $true
+
+do {
+    Mostrar-Menu
+    $opcion = Read-Host "Selecciona una opción (0-4)"
+
+    switch ($opcion) {
+        '1' { Ejecutar-Script "https://raw.githubusercontent.com/Vaquerow/Oryzon-SS/refs/heads/main/JarParser.ps1" }
+        '2' { Ejecutar-Script "https://raw.githubusercontent.com/Vaquerow/Oryzon-SS/refs/heads/main/BamParser.ps1" }
+        '3' { Ejecutar-Script "" }
+        '4' { Ejecutar-Script "" }
+        '0' {
+            Write-Host "Saliendo, Adios!" -ForegroundColor DarkGreen
+            $seguir = $false
+        }
+        default {
+            Write-Host "Opción inválida. Intenta de nuevo." -ForegroundColor DarkRed
+        }
+    }
+
+    if ($seguir) {
+        Write-Host ""
+        Write-Host "Pulsa Enter para seguir"
+        [void][System.Console]::ReadLine()
+    }
+
+} while ($seguir)
